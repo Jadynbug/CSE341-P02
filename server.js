6949574a41b0adcfad9d28cc822bf1e4e0d5739f
Jadynbug <83982927+Jadynbug@users.noreply.github.com>
2023-11-04 16:22:00 -0500

    W08 05

--- server.js
+++ server.js
@@ -50,6 +50,16 @@ app.use(morgan('dev'));
 
 //body parser
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
 
 //init error handling
 app.use(errorHandling);
